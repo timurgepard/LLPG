3109badf3ae011010e02091de808e1e231555b32
timurgepard <timur-ishuov@rambler.ru>
2022-11-06 01:25:48 +0600

    Update actor_critic.py

--- actor_critic.py
+++ actor_critic.py
@@ -18,9 +18,9 @@ class _actor_network():
 
     def model(self):
         state = Input(shape=self.state_dim, dtype='float64')
-        x = Dense(100, activation=atanh, kernel_initializer=RU(-1/np.sqrt(self.state_dim),1/np.sqrt(self.state_dim)))(state)
+        x = Dense(75, activation=atanh, kernel_initializer=RU(-1/np.sqrt(self.state_dim),1/np.sqrt(self.state_dim)))(state)
         #x = BatchNormalization()(state)
-        x = Dense(75, activation=atanh, kernel_initializer=RU(-1/np.sqrt(100),1/np.sqrt(100)))(x)
+        x = Dense(50, activation=atanh, kernel_initializer=RU(-1/np.sqrt(100),1/np.sqrt(100)))(x)
         #x = BatchNormalization()(x)
         out = Dense(self.action_dim, activation='tanh',kernel_initializer=RU(-0.003,0.003))(x)
         return Model(inputs=state, outputs=out)
@@ -33,11 +33,11 @@ class _critic_network():
 
     def model(self):
         state = Input(shape=self.state_dim, name='state_input', dtype='float64')
-        state_i = Dense(100, activation=atanh, kernel_initializer=RU(-1/np.sqrt(self.state_dim),1/np.sqrt(self.state_dim)))(state)
+        state_i = Dense(75, activation=atanh, kernel_initializer=RU(-1/np.sqrt(self.state_dim),1/np.sqrt(self.state_dim)))(state)
         #x = BatchNormalization()(state_i)
         action = Input(shape=(self.action_dim,), name='action_input')
         x = concatenate([state_i, action])
-        x = Dense(75, activation=atanh, kernel_initializer=RU(-1/np.sqrt(101),1/np.sqrt(101)))(x)
+        x = Dense(50, activation=atanh, kernel_initializer=RU(-1/np.sqrt(101),1/np.sqrt(101)))(x)
         #x = BatchNormalization()(x)
         out = Dense(1, activation='linear')(x)
         return Model(inputs=[state, action], outputs=out)
