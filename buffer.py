10f56f1280a918e8b34985b8b6c1b29c1dda782a
timurgepard <timur-ishuov@rambler.ru>
2022-11-13 03:33:26 +0600

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -16,16 +16,15 @@ class Record:
         self.batch_size = batch_size
         self.buffer = deque(maxlen=max_buffer_size)
 
-    def add_experience(self, state, action, reward, Q, next_state, next_Q):
-        self.buffer.append([state, action, reward, Q, next_state, next_Q])
+    def add_experience(self, state, action, reward, Q, next_state):
+        self.buffer.append([state, action, reward, Q, next_state])
 
-    def sample_batch(self):
+    def sample(self):
+        #arr = np.array(random.sample(self.buffer, self.batch_size)+[self.buffer[indx-1] for indx in range(-4,0)])
         arr = np.array(random.sample(self.buffer, self.batch_size))
         states_batch = np.vstack(arr[:, 0])
-        actions_batch = np.array(list(arr[:, 1]))
+        actions_batch =np.array(list(arr[:, 1]))
         rewards_batch = np.vstack(arr[:, 2])
         Q_batch = np.vstack(arr[:, 3])
         next_states_batch = np.vstack(arr[:, 4])
-        Q_next_batch = np.vstack(arr[:, 5])
-
-        return states_batch, actions_batch, rewards_batch, Q_batch, next_states_batch, Q_next_batch
+        return states_batch, actions_batch, rewards_batch, Q_batch, next_states_batch
