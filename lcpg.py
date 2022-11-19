6d5ae0f7aaedac8f83e1f21e60b54d40e76d5f33
timurgepard <timur-ishuov@rambler.ru>
2022-11-20 03:44:08 +0600

    Update lcpg.py

--- lcpg.py
+++ lcpg.py
@@ -141,9 +141,9 @@ class DDPG():
                 #At is a sample from normal dist
                 At = tf.random.normal(A.shape, 0.0, std[0])
                 #calculate log(Guassian dist)=log_prob, gauss const = log(1/sqrt(2pi))
-                log_prob = self.gauss_const-tf.math.log(std)-((A-At)/std)**2
+                log_prob = self.gauss_const-tf.math.log(std)-((A-At)/std)**2 - tf.math.reduce_sum(tf.math.log(1-tf.math.tanh(At)**2))
                 if self.type=="SAC":
-                    Q = Q*(1-0.05*log_prob) #5% of R => log_prob entropy
+                    Q = Q*(1-0.01*log_prob) #1% of R => log_prob entropy
                 elif self.type=="GAE":
                     V = VNN(St)
                     Q = log_prob*(Q-V) # log_prob now directs sign of gradient
