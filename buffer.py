f27451fc2b0d2e2adc53dcd9ede543cabb7d9bc7
timurgepard <timur-ishuov@rambler.ru>
2022-11-28 08:47:45 +0600

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -10,41 +10,42 @@ tf.get_logger().setLevel(logging.ERROR)
 def normalize(val, min, max):
     return (val - min)/(max - min)
 
-class Record:
-    def __init__(self, max_buffer_size, max_tape_size, batch_size):
+class Replay:
+    def __init__(self, max_buffer_size, max_record_size, batch_size):
         self.max_buffer_size = max_buffer_size
-        self.max_tape_size = max_tape_size
+        self.max_record_size = max_record_size
         self.batch_size = batch_size
-        self.buffer = deque(maxlen=max_buffer_size)
-        self.tape = deque(maxlen=max_tape_size)
-        self.priorites = deque(maxlen=max_tape_size)
-        self.indexes = deque(maxlen=max_tape_size)
-        self.stack_priorities = []
+        self.record = deque(maxlen=max_record_size)
+        self.cache = []
 
-    def add_roll_outs(self, state, R):
-        self.tape.append([state, R])
-        self.priorites.append(1.0)
-        ln = len(self.tape)
-        if ln <= self.max_tape_size: self.indexes.append(ln-1)
+    #transitions are saved in temporary memory cache, as soon as record is filled, cached is emptified
+    def add_transition(self, transition):
+        self.cache.append(transition)
 
-    def add_priorities(self,indices,priorites):
-        for idx,priority in zip(indices,priorites):
-            self.priorites[idx]=priority
+    #instead of one transition roll-out is saved for current time step for future steps with length = batch_size
+    def add_roll_outs(self, roll_out):
+        self.record.append(roll_out)
 
-    def sample(self):
-        arr = np.array(random.sample(self.buffer, self.batch_size))
-        states_batch = np.vstack(arr[:, 0])
-        actions_batch =np.array(list(arr[:, 1]))
-        rewards_batch = np.vstack(arr[:, 2])
-        next_states_batch = np.vstack(arr[:, 3])
-        st_dev_batch = np.vstack(arr[:, 4])
-        done_batch = np.vstack(arr[:, 5])
-        return states_batch, actions_batch, rewards_batch, next_states_batch, st_dev_batch, done_batch
+    def restore(self, n_step, gamma):
+        #combined exp replay: add last n steps to batch: Exp Replay < Combined Exp Replay < Prioritized Exp Replay
+        cer = random.sample(self.record, self.batch_size-n_step)+[self.record[indx-1] for indx in range(-n_step,0)]
+        #roll-outs are retrieved here
+        arr = np.array(cer)
+        Sts =  np.vstack(arr[:, 0, :])
+        Ats = np.vstack(arr[:, 1, :])
+        rts = np.vstack(arr[:, 2, :])
+        Sts_ = np.vstack(arr[:, 3, :])
+        Ts = np.vstack(arr[:, 4])
 
-    def restore(self):
-        arr, indices = np.array(random.sample(self.buffer, self.batch_size)), []
-        #indices = random.choices(self.indexes, weights=self.priorites, k = self.batch_size)
-        #arr = np.array([self.tape[indx-1] for indx in indices])
-        states_batch = np.vstack(arr[:, 0])
-        return_batch = np.vstack(arr[:, 1])
-        return states_batch, return_batch, indices
+        # n-step related batch transition
+        Qt = np.zeros((self.batch_size,1))
+        for t in range(n_step):
+            Qt += gamma**t*np.vstack(rts[:,t]) # here Q is calcualted
+        St0 = np.vstack(Sts[:,0])
+        At0 = np.vstack(Ats[:,0])
+        Stn = np.vstack(Sts[:,n_step-1])
+        Atn = np.vstack(Ats[:,n_step-1])
+        Stn_ = np.vstack(Sts_[:,n_step-1])
+        Tn = np.vstack(Ts[:,n_step-1])
+
+        return St0, At0, Stn, Atn, Stn_, Tn, Qt
