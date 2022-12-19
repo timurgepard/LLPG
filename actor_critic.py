89d7c70e61121c0169a732cf54dd9de54c370534
timurgepard <timur-ishuov@rambler.ru>
2022-12-19 18:30:46 +0600

    Update actor_critic.py

--- actor_critic.py
+++ actor_critic.py
@@ -1,9 +1,9 @@
 import numpy as np
 from tensorflow.keras.initializers import RandomUniform as RU
-from tensorflow.keras.regularizers import l2
 from tensorflow.keras.layers import Dense, Input, concatenate, BatchNormalization, Dropout, SimpleRNN
 from tensorflow.keras import Model
 from tensorflow.keras import backend as K
+from tensorflow_addons.layers import NoisyDense
 import tensorflow as tf
 import logging
 tf.get_logger().setLevel(logging.ERROR)
@@ -18,12 +18,10 @@ class _actor_network():
         self.action_bound_range = action_bound_range
 
     def model(self):
-        state = Input(shape=(self.state_dim), dtype='float64')
-        x = Dense(400, activation='relu', kernel_initializer=RU(-1/np.sqrt(self.state_dim),1/np.sqrt(self.state_dim)))(state)
-        x = concatenate([state, x])
-        x = Dense(300, activation='relu', kernel_initializer=RU(-1/np.sqrt(40),1/np.sqrt(40)))(x)
-        x = concatenate([state, x])
-        out = Dense(self.action_dim, activation='tanh', kernel_initializer=RU(-0.003,0.003))(x)
+        state = Input(shape=self.state_dim, dtype='float64')
+        x = NoisyDense(100, sigma=0.01, activation=atanh, kernel_initializer=RU(-1/np.sqrt(self.state_dim),1/np.sqrt(self.state_dim)))(state)
+        x = NoisyDense(75, sigma=0.01, activation=atanh, kernel_initializer=RU(-1/np.sqrt(100),1/np.sqrt(100)))(x)
+        out = Dense(self.action_dim, activation='tanh',kernel_initializer=RU(-0.003,0.003))(x)
         return Model(inputs=state, outputs=out)
 
 
@@ -34,10 +32,9 @@ class _q_network():
 
     def model(self):
         state = Input(shape=self.state_dim, name='state_input', dtype='float64')
-        x = Dense(400, activation='relu', kernel_initializer=RU(-1/np.sqrt(self.state_dim),1/np.sqrt(self.state_dim)))(state)
-        action = Input(shape=(self.action_dim,), name='action_input', dtype='float64')
-        x = concatenate([state, action, x])
-        x = Dense(300, activation='relu', kernel_initializer=RU(-1/np.sqrt(40),1/np.sqrt(40)))(x)
-        x = concatenate([state, action, x])
-        out = Dense(1, activation='linear', kernel_initializer=RU(-0.003,0.003))(x)
+        state_i = NoisyDense(100, sigma=0.01, activation=atanh, kernel_initializer=RU(-1/np.sqrt(self.state_dim),1/np.sqrt(self.state_dim)))(state)
+        action = Input(shape=(self.action_dim,), name='action_input')
+        x = concatenate([state_i, action])
+        x = NoisyDense(75, sigma=0.01, activation=atanh, kernel_initializer=RU(-1/np.sqrt(101),1/np.sqrt(101)))(x)
+        out = Dense(1, activation='linear')(x)
         return Model(inputs=[state, action], outputs=out)
