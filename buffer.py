60911a1833853dfb6ace263a06ce32d77d086d64
timurgepard <timur-ishuov@rambler.ru>
2022-12-19 18:30:03 +0600

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -11,54 +11,33 @@ def normalize(val, min, max):
     return (val - min)/(max - min)
 
 class Replay:
-    def __init__(self, max_buffer_size, max_record_size, batch_size):
+    def __init__(self, max_record_size, batch_size):
         self.max_record_size = max_record_size
         self.batch_size = batch_size
-        self.sample_size = 20*self.batch_size
-        self.record = deque(maxlen=max_record_size)
+        self.buffer = deque(maxlen=max_record_size)
         self.cache = []
-        self.priorites = deque(maxlen=max_buffer_size)
-        self.indexes = deque(maxlen=max_buffer_size)
 
-    #transitions are saved in temporary memory cache, as soon as record is filled, cached is emptified
-    def add_transition(self, transition):
-        self.cache.append(transition)
 
-    def add_priorities(self,indices,priorites):
-        for idx,priority in zip(indices,priorites):
-            self.priorites[idx]=priority[0].numpy()
-
-    #instead of one transition roll-out is saved for current time step for future steps with length = batch_size
     def add_roll_outs(self, roll_out):
-        self.record.append(roll_out)
-        self.priorites.append(100.0)
-        ln = len(self.record)
-        if ln <= self.max_record_size: self.indexes.append(ln-1)
+        self.buffer.append(roll_out)
 
-    def restore(self, n_step, gamma):
-        #combined exp replay: add last n steps to batch:
-        #cer = random.sample(self.record, self.batch_size-n_step)+[self.record[indx-1] for indx in range(-n_step,0)]
-        #roll-outs are retrieved here
-        sample = np.random.default_rng().choice(self.indexes, size=self.sample_size, replace=False)
-        priorites = np.array(self.priorites)[sample]
-        indices = np.random.default_rng().choice(sample, size=self.batch_size, p=priorites/np.sum(priorites), replace=False)
-        arr = np.array([self.record[indx-1] for indx in indices])
 
-        Sts =  np.vstack(arr[:, 0, :])
-        Ats = np.vstack(arr[:, 1, :])
-        rts = np.vstack(arr[:, 2, :])
-        Sts_ = np.vstack(arr[:, 3, :])
-        Ts = np.vstack(arr[:, 4])
+    def restore(self, n_steps, gamma):
+        if len(self.buffer)>=self.batch_size:
+            arr = np.random.default_rng().choice(self.buffer, size=self.batch_size, replace=False)
 
-        # n-step related batch transition
-        Qt = np.zeros((self.batch_size,1))
-        for t in range(n_step):
-            Qt += gamma**t*np.vstack(rts[:,t]) # here Q is calcualted
-        St0 = np.vstack(Sts[:,0])
-        At0 = np.vstack(Ats[:,0])
-        Stn = np.vstack(Sts[:,n_step-1])
-        Atn = np.vstack(Ats[:,n_step-1])
-        Stn_ = np.vstack(Sts_[:,n_step-1])
-        Tn = np.vstack(Ts[:,n_step-1])
+            Sts =  np.vstack(arr[:, 0, :])
+            Ats = np.vstack(arr[:, 1, :])
+            rts = np.vstack(arr[:, 2, :])
+            Sts_ = np.vstack(arr[:, 3, :])
 
-        return St0, At0, Stn, Atn, Stn_, Tn, Qt, indices
+            Ql = Qt = np.zeros((self.batch_size,1))
+            for t in range(n_steps):
+                Qt += gamma**t*np.vstack(rts[:,t]) # here Q is calcualted
+                if t<n_steps-1: Ql += 0.1*0.95**t*Qt
+            Ql += 0.95**(n_steps-1)*Qt
+            St = np.vstack(Sts[:,0])
+            At = np.vstack(Ats[:,0])
+            St_ = np.vstack(Sts_[:, n_steps-1])
+
+        return St, At, Ql, St_
